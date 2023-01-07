4e70291728f0f4c6038e88d411b2d82bc33a48b6
Omer <omerarviv801@gmail.com>
2023-01-07 03:37:46 +0200

    devwork - test7

--- container.ps1
+++ container.ps1
@@ -10,6 +10,7 @@ $acInfo1 = @(az storage account keys list --account-name omerstorage1 | ConvertF
 $acKey1 = $acInfo1[0].value
 write-output $acInfo1[0]
 
+Write-Output "____________________"
 $acInfo2 = @(az storage account keys list --account-name omerstorage2 | ConvertFrom-Json)
 $acKey2 = $acInfo2[0].value
 write-output $acInfo2[0]
